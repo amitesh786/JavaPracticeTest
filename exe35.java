4572dcbfea23f639376d27acf136710b1bb89d6d
amitesh786 <amitesh.jy.786@gmail.com>
2018-05-30 17:55:20 +0200

    [add] Practice exe

---
+++ exe35.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import static java.lang.System.out;
+import java.lang.Object;
+import java.lang.Number;
+import java.lang.Integer;
+import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+
+class exe35 {
+
+    public static void main(String[] args) {
+        
+        
+	}
+}
