a4c284334dd6b6ea270528b98e50011d3f2b91df
amitesh786 <amitesh.jy.786@gmail.com>
2018-06-04 17:15:19 +0200

    [add] First & Last element equal in am array

---
+++ exe38.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import static java.lang.System.out;
+import java.lang.Object;
+import java.lang.Number;
+import java.lang.Integer;
+import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+
+class exe38 {
+    public static void main(String[] args) {
+
+        int[] arr = {11, 1, 13, 10, 15, 40, 30, 33, 112, 11};
+        out.println("Enter the array: " + Arrays.toString(arr));
+        int result = 1;
+        int first, last;
+            
+        if(arr.length != 0) {
+            first = arr[0];
+            last = arr[arr.length - 1];
+
+            out.println("First: " + first);
+            out.println("Last: " + last);
+
+            if(first == last) {
+                result = 0;
+            }
+        }
+
+        if(result == 0) {
+            out.println("Array: " + "true");
+        } else {
+            out.println("Array: " + "false");
+        }
+	}
+}
